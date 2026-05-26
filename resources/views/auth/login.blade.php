36482728312e109a5518a28a074e7689d3034305
CodeBySalsa <saa22bila@gmail.com>
2026-05-26 20:34:52 +0700

    chore: cleanup old build assets

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,9 +26,8 @@
     <div class="login-card">
 
         <div class="logo-box">
-            <img src="{{ asset('logo pt salttek dumpang jaya.jpeg') }}" alt="Logo">
-        </div>
-
+    <img src="{{ asset('logo.jpeg') }}" alt="Logo PT Salttek">
+</div>
         <h1>
             PT SALTTEK
             <span>DUMPANG JAYA</span>
